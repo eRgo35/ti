7746678538a2b5e305fc83853314c7e42a9a1afd
Michał Czyż <mike@c2yz.com>
2024-08-11 15:08:12 +0200

    refactor: moved drawing to separate mod

---
+++ src/interface.rs
@@ -0,0 +1,106 @@
+use std::{
+    io::{stdout, Write},
+    process::exit,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use crossterm::{
+    cursor::{Hide, MoveTo},
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    style::Print,
+    terminal::{self, Clear, ClearType},
+    QueueableCommand,
+};
+use figlet_rs::FIGfont;
+
+use crate::timer::Timer;
+
+const REFRESH_RATE: Duration = Duration::from_millis(100);
+const FONT_WIDTH: usize = 9;
+
+fn handle_exit(code: i32) {
+    terminal::disable_raw_mode().unwrap();
+    println!();
+    exit(code);
+}
+
+fn handle_keyboard() -> Option<char> {
+    if let Ok(Event::Key(KeyEvent {
+        code, modifiers, ..
+    })) = event::read()
+    {
+        match (code, modifiers) {
+            (KeyCode::Char('c'), KeyModifiers::CONTROL) => handle_exit(1),
+            (KeyCode::Char(c), _) => return Some(c.to_ascii_lowercase()),
+            _ => {}
+        }
+    }
+    None
+}
+
+fn draw_timer(time: &str, font: &FIGfont) {
+    let figure = font.convert(time).unwrap();
+
+    // TODO: Calculate figure width and height properly
+    let figure_width = (time.len() * FONT_WIDTH) as u16;
+    let figure_height = figure.to_string().lines().count() as u16;
+
+    let (terminal_width, terminal_height) = terminal::size().unwrap();
+
+    let x = terminal_width
+        .saturating_sub(figure_width)
+        .saturating_div(2);
+    let y = terminal_height
+        .saturating_sub(figure_height)
+        .saturating_div(2);
+
+    let mut stdout = stdout();
+
+    execute!(stdout, Hide).unwrap();
+
+    stdout.queue(Clear(ClearType::All)).unwrap();
+
+    for (i, line) in figure.to_string().lines().enumerate() {
+        stdout.queue(MoveTo(x, y + i as u16)).unwrap();
+        stdout.queue(Print(line)).unwrap();
+    }
+
+    stdout.queue(MoveTo(0, terminal_height)).unwrap();
+
+    stdout
+        .queue(Print("SPACE [Pause/Resume] | R [Reset] | Q [Quit]"))
+        .unwrap();
+
+    stdout.flush().unwrap();
+}
+
+pub fn handle_interface(timer: Arc<Mutex<Timer>>, font: FIGfont) {
+    terminal::enable_raw_mode().unwrap();
+
+    loop {
+        if event::poll(REFRESH_RATE).unwrap() {
+            if let Some(key) = handle_keyboard() {
+                let mut timer = timer.lock().unwrap();
+                match key {
+                    'q' => {
+                        timer.stop();
+                        break;
+                    }
+                    's' => timer.start(),
+                    'p' => timer.pause(),
+                    ' ' => timer.toggle(),
+                    'r' => timer.reset(),
+                    _ => {}
+                }
+            }
+        }
+
+        let time = timer.lock().unwrap().print();
+        draw_timer(&time, &font);
+    }
+
+    terminal::disable_raw_mode().unwrap();
+    println!("\nQuitting...");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,20 @@
 mod args;
 mod cache;
 mod font;
+mod interface;
 mod timer;
-
-use std::{
-    io::{stdout, Write},
-    process::exit,
-    sync::{Arc, Mutex},
-    thread,
-    time::Duration,
-};
-
-use crossterm::{
-    cursor::{Hide, MoveTo},
-    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
-    execute,
-    style::Print,
-    terminal::{self, Clear, ClearType},
-    QueueableCommand,
-};
 
 use args::Args;
 use cache::Cache;
 use clap::Parser;
-use figlet_rs::FIGfont;
 use font::load_font;
+use interface::handle_interface;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 use timer::Timer;
-
-const FONT_WIDTH: usize = 9;
-const REFRESH_RATE: Duration = Duration::from_millis(100);
-
-fn draw_timer(time: &str, font: &FIGfont) {
-    let figure = font.convert(time).unwrap();
-
-    // TODO: Calculate figure width and height properly
-    let figure_width = (time.len() * FONT_WIDTH) as u16;
-    let figure_height = figure.to_string().lines().count() as u16;
-
-    let (terminal_width, terminal_height) = terminal::size().unwrap();
-
-    let x = terminal_width
-        .saturating_sub(figure_width)
-        .saturating_div(2);
-    let y = terminal_height
-        .saturating_sub(figure_height)
-        .saturating_div(2);
-
-    let mut stdout = stdout();
-
-    execute!(stdout, Hide).unwrap();
-
-    stdout.queue(Clear(ClearType::All)).unwrap();
-
-    for (i, line) in figure.to_string().lines().enumerate() {
-        stdout.queue(MoveTo(x, y + i as u16)).unwrap();
-        stdout.queue(Print(line)).unwrap();
-    }
-
-    stdout.queue(MoveTo(0, terminal_height)).unwrap();
-
-    stdout
-        .queue(Print("SPACE [Pause/Resume] | R [Reset] | Q [Quit]"))
-        .unwrap();
-
-    stdout.flush().unwrap();
-}
-
-fn handle_exit(code: i32) {
-    terminal::disable_raw_mode().unwrap();
-    println!();
-    exit(code);
-}
-
-fn handle_keyboard() -> Option<char> {
-    if let Ok(Event::Key(KeyEvent {
-        code, modifiers, ..
-    })) = event::read()
-    {
-        match (code, modifiers) {
-            (KeyCode::Char('c'), KeyModifiers::CONTROL) => handle_exit(1),
-            (KeyCode::Char(c), _) => return Some(c.to_ascii_lowercase()),
-            _ => {}
-        }
-    }
-    None
-}
 
 fn main() {
     let Args {
@@ -97,7 +27,6 @@ fn main() {
 
     let cache = Cache::new(cache);
     let font = load_font(font);
-
     let timer = Arc::new(Mutex::new(
         cache
             .load()
@@ -107,7 +36,6 @@ fn main() {
 
     let timer_clone = Arc::clone(&timer);
     let cache_clone = cache.clone();
-
     let countdown_thread = thread::spawn(move || loop {
         thread::sleep(Duration::from_secs(1));
         let mut timer = timer_clone.lock().unwrap();
@@ -118,32 +46,7 @@ fn main() {
         }
     });
 
-    terminal::enable_raw_mode().unwrap();
-
-    loop {
-        if event::poll(REFRESH_RATE).unwrap() {
-            if let Some(key) = handle_keyboard() {
-                let mut timer = timer.lock().unwrap();
-                match key {
-                    'q' => {
-                        timer.stop();
-                        break;
-                    }
-                    's' => timer.start(),
-                    'p' => timer.pause(),
-                    ' ' => timer.toggle(),
-                    'r' => timer.reset(),
-                    _ => {}
-                }
-            }
-        }
-
-        let time = timer.lock().unwrap().print();
-        draw_timer(&time, &font);
-    }
-
-    terminal::disable_raw_mode().unwrap();
-    println!("\nQuitting...");
+    handle_interface(timer, font);
 
     countdown_thread.join().unwrap();
     cache.clear();
